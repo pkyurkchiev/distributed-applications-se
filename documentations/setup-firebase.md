c7fcad71cd1d8782173fbeadbb84774eff2af3cd
Pavel Kyurkchiev <pavkyu@gmail.com>
2018-06-02 16:53:37 +0300

    week nine

---
+++ documentations/setup-firebase.md
@@ -0,0 +1,25 @@
+# Google Firebase
+Setup Google Firebase
+
+
+
+#
+### Steps
+
+* Step One - install nodejs: 
+  * [Download link](nodejs.org)
+
+
+  
+* Step two - install firebase: 
+  * Command Line - npm install -g firebase-tools
+
+
+
+* Step three - login: 
+  * Command Line - firebase login
+  
+  
+  
+* Step fourth - setup firebase project: 
+  * Command Line - firebase init funtions
---
+++ exercises/07/insertIntoDatabase-functions-js/functions/index.js
@@ -0,0 +1,32 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+exports.addMessage = functions.https.onRequest((req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text;
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    return res.redirect(303, snapshot.ref.toString());
+  });
+});
+
+// Listens for new messages added to /messages/:pushId/original and creates an
+// uppercase version of the message to /messages/:pushId/uppercase
+exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
+    .onCreate((snapshot, context) => {
+      // Grab the current value of what was written to the Realtime Database.
+      const original = snapshot.val();
+      console.log('Uppercasing', context.params.pushId, original);
+      const uppercase = original.toUpperCase();
+      // You must return a Promise when performing asynchronous tasks inside a Functions such as
+      // writing to the Firebase Realtime Database.
+      // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+      return snapshot.ref.parent.child('uppercase').set(uppercase);
+    });
