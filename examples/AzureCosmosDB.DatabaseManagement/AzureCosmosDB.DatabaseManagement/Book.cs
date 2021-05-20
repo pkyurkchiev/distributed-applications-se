f7f7a47dba637d1663b84dfa56d3debb455f1c92
Pavel Kyurkchiev <pavkyu@gmail.com>
2021-05-20 10:36:31 +0300

    week 7

---
+++ examples/AzureCosmosDB.DatabaseManagement/AzureCosmosDB.DatabaseManagement/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureCosmosDB.DatabaseManagement
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string ISBN { get; set; }
+        public List<string> Authors  { get; set; }
+        public int PageCount { get; set; }
+    }
+}
---
+++ examples/DinosaurCatalog/DC.Console/Dinosaur.cs
@@ -0,0 +1,7 @@
+﻿namespace DC.Treminal
+{
+    public class Dinosaur
+    {
+        public double Height { get; set; }
+    }
+}
