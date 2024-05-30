ff8f5d6306b19fbb6d41b095c95c6642523508b9
Pavel Kyurkchiev <pavkyu@gmail.com>
2024-05-30 17:32:28 +0300

    week 7

---
+++ examples/AzureCosmosDB.DatabaseManagement/AzureCosmosDB.DatabaseManagement/Program.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Azure.Cosmos;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AzureCosmosDB.DatabaseManagement
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string databaseId = "shop-database";
+            string containerId = "books";
+            string connectionString = "AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+
+            // Connect to the Azure Cosmos Emulator running locally
+            using (CosmosClient cosmosClient = new(connectionString))
+            {
+                // Create database
+                Database database = await CreateDatabaseAsync(cosmosClient, databaseId);
+
+                // Create collection
+                Container container = await CreateContainerAsync(database, containerId);
+
+                string bookId = Guid.NewGuid().ToString();
+                Book book = new()
+                {
+                    Id = bookId,
+                    Title = "Book " + bookId,
+                    Price = 19.99m,
+                    ISBN = "111-1111111111",
+                    Authors = ["Author 1", "Author 2", "Author 3"],
+                    PageCount = 500
+                };
+
+                // Create document
+                await CreateBook(container, book);
+
+                // Query Book by id
+                await QueryBookAsync(container, book.ISBN);
+
+                // Delete Book
+                await DeleteBookAsync(container, book.Id, book.ISBN);
+
+                // Delete database
+                await DeleteDatabaseAsync(database, databaseId);
+            }
+        }
+
+        private static async Task<Database> CreateDatabaseAsync(CosmosClient cosmosClient, string databaseId)
+        {
+            Database database = await cosmosClient.CreateDatabaseIfNotExistsAsync(databaseId);
+            Console.WriteLine("Created Database: {0}\n", database == null ? "no database" : database.Id);
+            return database;
+        }
+
+        private static async Task<Container> CreateContainerAsync(Database database, string containerId)
+        {
+            // Create a new container
+            Container container = await database.CreateContainerIfNotExistsAsync(containerId, "/ISBN");
+            Console.WriteLine("Created Container: {0}\n", container.Id);
+            return container;
+        }
+
+        private static async Task CreateBook(Container container, Book book)
+        {
+            ItemResponse<Book> bookResponse = await container.CreateItemAsync<Book>(book, new PartitionKey(book.ISBN));
+
+            Console.WriteLine("Created item in database with id: {0} Operation consumed {1} RUs.\n", bookResponse.Resource.Id, bookResponse.RequestCharge);
+        }
+
+        private static async Task QueryBookAsync(Container container, string isbn)
+        {
+            var sqlQueryText = $"SELECT * FROM c WHERE c.ISBN = {isbn}";
+
+            Console.WriteLine("Running query: {0}\n", sqlQueryText);
+
+            QueryDefinition queryDefinition = new(sqlQueryText);
+            using FeedIterator<Book> queryResultSetIterator = container.GetItemQueryIterator<Book>(queryDefinition);
+
+            List<Book> books = [];
+
+            while (queryResultSetIterator.HasMoreResults)
+            {
+                FeedResponse<Book> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+                foreach (Book book in currentResultSet)
+                {
+                    books.Add(book);
+                    Console.WriteLine("\tRead {0}\n", book);
+                }
+            }
+        }
+
+        private static async Task DeleteBookAsync(Container container, string bookId, string isbn)
+        {
+            _ = await container.DeleteItemAsync<Book>(bookId, new PartitionKey(isbn));
+            Console.WriteLine("Deleted Book [{0}]\n", bookId);
+        }
+
+        private static async Task DeleteDatabaseAsync(Database database, string databasseId)
+        {
+            _ = await database.DeleteAsync();
+
+            Console.WriteLine("Deleted Database: {0}\n", databasseId);
+        }
+    }
+}
---
+++ examples/AzureDataStorage.Management/ADS.QueueManagement/Program.cs
@@ -0,0 +1,161 @@
+﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using System;
+
+namespace ADS.QueueManagement
+{
+    class Program
+    {
+        private const string connectionString = "UseDevelopmentStorage=true";
+        static void Main(string[] args)
+        {
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            // Create the queue name -- use a guid in the name so it's unique.
+            string queueName = $"demotest-{Guid.NewGuid()}";
+
+            // Create or reference an existing queue.
+            CreateQueue(queueName);
+
+            // Demonstrate basic queue functionality.  
+            InsertMessage(queueName, "Hello");
+            PeekMessage(queueName);
+            DequeueMessage(queueName);
+            GetQueueLength(queueName);
+
+            // Delete the queue uncomment the line of code below.  
+            DeleteQueue(queueName);
+        }
+
+        //-------------------------------------------------
+        // Create a message queue
+        //-------------------------------------------------
+        public static bool CreateQueue(string queueName)
+        {
+            try
+            {
+                // Instantiate a QueueClient which will be used to create and manipulate the queue
+                QueueClient queueClient = new(connectionString, queueName);
+
+                // Create the queue
+                queueClient.CreateIfNotExists();
+
+                if (queueClient.Exists())
+                {
+                    Console.WriteLine($"Queue created: '{queueClient.Name}'");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine($"Make sure the Azurite storage emulator running and try again.");
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}\n\n");
+                Console.WriteLine($"Make sure the Azurite storage emulator running and try again.");
+                return false;
+            }
+        }
+
+        //-------------------------------------------------
+        // Insert a message into a queue
+        //-------------------------------------------------
+        public static void InsertMessage(string queueName, string message)
+        {
+            // Instantiate a QueueClient which will be used to create and manipulate the queue
+            QueueClient queueClient = new(connectionString, queueName);
+
+            // Create the queue if it doesn't already exist
+            queueClient.CreateIfNotExists();
+
+            if (queueClient.Exists())
+            {
+                // Send a message to the queue
+                queueClient.SendMessage(message);
+            }
+
+            Console.WriteLine($"Inserted: {message}");
+        }
+
+        //-------------------------------------------------
+        // Peek at a message in the queue
+        //-------------------------------------------------
+        public static void PeekMessage(string queueName)
+        {
+            // Instantiate a QueueClient which will be used to manipulate the queue
+            QueueClient queueClient = new(connectionString, queueName);
+
+            if (queueClient.Exists())
+            {
+                // Peek at the next message
+                PeekedMessage[] peekedMessage = queueClient.PeekMessages();
+
+                // Display the message
+                Console.WriteLine($"Peeked message: '{peekedMessage[0].MessageText}'");
+            }
+        }
+
+        //-------------------------------------------------
+        // Process and remove a message from the queue
+        //-------------------------------------------------
+        public static void DequeueMessage(string queueName)
+        {
+            // Instantiate a QueueClient which will be used to manipulate the queue
+            QueueClient queueClient = new(connectionString, queueName);
+
+            if (queueClient.Exists())
+            {
+                // Get the next message
+                QueueMessage[] retrievedMessage = queueClient.ReceiveMessages();
+
+                // Process (i.e. print) the message in less than 30 seconds
+                Console.WriteLine($"Dequeued message: '{retrievedMessage[0].MessageText}'");
+
+                // Delete the message
+                queueClient.DeleteMessage(retrievedMessage[0].MessageId, retrievedMessage[0].PopReceipt);
+            }
+        }
+
+        //-----------------------------------------------------
+        // Get the approximate number of messages in the queue
+        //-----------------------------------------------------
+        public static void GetQueueLength(string queueName)
+        {
+            // Instantiate a QueueClient which will be used to manipulate the queue
+            QueueClient queueClient = new(connectionString, queueName);
+
+            if (queueClient.Exists())
+            {
+                QueueProperties properties = queueClient.GetProperties();
+
+                // Retrieve the cached approximate message count.
+                int cachedMessagesCount = properties.ApproximateMessagesCount;
+
+                // Display number of messages.
+                Console.WriteLine($"Number of messages in queue: {cachedMessagesCount}");
+            }
+        }
+
+        //-------------------------------------------------
+        // Delete the queue
+        //-------------------------------------------------
+        public static void DeleteQueue(string queueName)
+        {
+            // Instantiate a QueueClient which will be used to manipulate the queue
+            QueueClient queueClient = new(connectionString, queueName);
+
+            if (queueClient.Exists())
+            {
+                // Delete the queue
+                queueClient.Delete();
+            }
+
+            Console.WriteLine($"Queue deleted: '{queueClient.Name}'");
+        }
+    }
+}
